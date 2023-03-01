8e8ab374c7580f3b2e1a10fe99c230a7f03e6d5e
vijaycharan1608 <125926189+vijaycharan1608@users.noreply.github.com>
2023-02-28 22:02:43 -0500

    added snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-
-
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
@@ -38,3 +36,24 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+# snowflake connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
+my_cur = my_cnx.cursor()
+
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+
+my_data_row = my_cur.fetchone()
+
+streamlit.text("Hello from Snowflake:")
+
+streamlit.text(my_data_row)
+
+
+
+
+
+
+
+
