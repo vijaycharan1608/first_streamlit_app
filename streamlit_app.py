82bbcf632b78ef3adf22fe972d5810ea7e710379
vijaycharan1608 <125926189+vijaycharan1608@users.noreply.github.com>
2023-02-28 22:13:30 -0500

    Fix error

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -36,6 +37,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.stop()
 
 # snowflake connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
